50c52f476a33972758854683d465bf46ece3f5ad
Halil Gedik <halilgedik06@gmail.com>
2022-01-25 14:14:46 +0300

    Add project files.

---
+++ CoreWebSite/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using CoreWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public IActionResult Servis()
+        {
+            return View();
+        }
+
+        public IActionResult Galeri()
+        {
+            return View();
+        }
+
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Iletisim(Mail mail)
+        {
+            try
+            {
+                
+                var client = new SmtpClient("smtp.gmail.com", 587);
+                client.UseDefaultCredentials = false;                
+                client.Credentials = new NetworkCredential("username", "password");
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                string mesaj = "Adı Soyadı : " + mail.Adi + " " + mail.Soyadi;
+                mesaj += "\nKonu : " + mail.Konu;
+                mesaj += "\nE-mail: " + mail.Email;
+                mesaj += "\nMesaj: " + mail.Mesaj;
+                client.Send("from_email", "to_email", mail.Konu, mesaj);
+
+                ViewBag.Success = "Mail başarılı bir şekilde gönderildi";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Hata oluştu";
+                return View();
+            }
+        }
+    }
+}
---
+++ CoreWebSite/Models/Mail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebSite.Models
+{
+    public class Mail
+    {
+        [Required]
+        public string Adi { get; set; }
+        [Required]
+        public string Soyadi{ get; set; }
+        [Required]
+        public string Email{ get; set; }
+        [Required]
+        public string Konu{ get; set; }
+        [Required]
+        public string Mesaj{ get; set; }
+    }
+}
